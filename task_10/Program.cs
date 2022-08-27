04ab2991dadd9b367bd6795192c04dc22d8d13c6
Ledsager <94687899+Ledsager@users.noreply.github.com>
2022-08-27 11:23:23 +0300

    deleted an unnecessary comment

--- task_10/Program.cs
+++ task_10/Program.cs
@@ -1,16 +1,8 @@
 ﻿// Найти третью цифру числа или сообщить, что её нет
+
 Console.WriteLine("Введите чиcло:");
 double num = double.Parse(Console.ReadLine());
 string a = num.ToString();// Преобразуем число в строку
-/*определение длины числа
-int n = 1;
-double reserv = num;
-while ((int)(reserv / 10) != 0)
-{
-    reserv = reserv / 10;
-    n++;
-}
-*/
 if (a.Length < 3)// или if (((int)Math.Log10((double)num) + 1) < 3)
 {
 Console.WriteLine($"Количество цифр в числе: {num} меньше 2х");
